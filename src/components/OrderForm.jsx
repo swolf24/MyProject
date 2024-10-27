04a739ec30d36c7a2a2b470045e61e165b0fc079
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 02:57:31 +0100

    add function ProductForm

---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import OrderInfo from './OrderInfo'; // Importiere OrderInfo-Komponente
+
+function ProductForm() {
+  const nameP = [
+    'Hairdryer Dyson',
+    'Revlon RVDR5222E',
+    'BaByliss Air Wand',
+  ];
+
+  const priceP = [
+    449,
+    35,
+    106,
+  ];
