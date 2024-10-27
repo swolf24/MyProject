41b0c25bbbfc0dee7cb08948d2bef46030986bcd
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 11:06:47 +0100

    add the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import React, { useState } from 'react';
 import './App.css';
-
-// Header Komponente
-function Header({ image, title }) {
-  return (
-    <header className="header">
-      <img src={image} alt="Header" className="header-image" />
-      <h1>{title}</h1>
-    </header>
-  );
-}
+import ComponentUI from './components/ComponentUI';
+import OrderForm from './components/OrderForm';
