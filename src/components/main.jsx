e4ef46f942f26dc15ce4d77679f6e48ae219da84
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 11:18:04 +0100

    add main

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom';
+import App from './App'; // Stelle sicher, dass der Pfad korrekt ist
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
