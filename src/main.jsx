94e6be9b1103ba09d07d862d13d51612c5e03ed7
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 11:21:13 +0100

    change the scr

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Stelle sicher, dass der Pfad korrekt ist
+import App from './components/App'; // Stelle sicher, dass der Pfad korrekt ist
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
