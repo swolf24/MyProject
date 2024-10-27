b3ea4a9a79c9a94d995e8f6caf996c0a7cb488ab
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 09:53:54 +0100

    add container to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,15 @@ import Header from './components/ComponentUI';
 import ProductForm from './components/OrderForm';
 import './App.css';
 
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <Header 
-        image="https://media.istockphoto.com/id/1171385777/de/foto/nettes-mädchen-schließen-ihre-augen-im-badezimmer.jpg?s=612x612&w=0&k=20&c=h-svPudCG_feNvf4nuchrPhRbQQuYqLmTS9f3o6IFV0=" 
-        title="Produkt-Auswahl" 
-      />
-      <ProductForm />
-
-      {/* Separate section for Vite and React logos */}
-      <div className="logos-section">
-      
+    <div className="container"> 
+      <div className="content-box"> 
+        <Header 
+          image="https://media.istockphoto.com/id/1171385777/de/foto/nettes-mädchen-schließen-ihre-augen-im-badezimmer.jpg?s=612x612&w=0&k=20&c=h-svPudCG_feNvf4nuchrPhRbQQuYqLmTS9f3o6IFV0=" 
+          title="Produkt-Auswahl" 
+        />
+        <ProductForm />
       </div>
     </div>
   );
