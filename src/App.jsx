8ecdbb0b94b607aebdc648135152c3725586017f
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 10:19:44 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './components/ComponentUI';
 import ProductForm from './components/OrderForm';
 import './App.css';
