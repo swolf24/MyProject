c02f72eb16997252d668bffe2452e4f74cdd20bc
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 13:06:13 +0100

    Removed routing for published version

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Header from './components/ComponentUI';
-import ProductForm from './components/OrderForm';
-import Home from './components/Home';
+import Header from './components/Header';
+import ProductForm from './components/ProductForm';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <div className="content-box">
-          <Header 
-            image="https://media.istockphoto.com/id/1171385777/de/foto/nettes-mädchen-schließen-ihre-augen-im-badezimmer.jpg?s=612x612&w=0&k=20&c=h-svPudCG_feNvf4nuchrPhRbQQuYqLmTS9f3o6IFV0=" 
-            title="Produkt-Auswahl" 
-          />
-          {/* Navigationslinks */}
-          <nav>
-            <Link to="/">Home</Link> | <Link to="/products">Products</Link>
-          </nav>
-          
-          {/* Definiere die Routen */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products" element={<ProductForm />} />
-          </Routes>
-        </div>
+    <div className="container">
+      <div className="content-box">
+        <Header 
+          image="https://example.com/image.jpg" 
+          title="Produkt-Auswahl" 
+        />
+        <ProductForm />
       </div>
-    </Router>
+    </div>
   );
 }
 
