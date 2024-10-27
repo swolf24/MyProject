151e32477e7a2a8d177088fef3fc5cbc004dcf7c
Sophie Wolf <s.maren.w@gmail.com>
2024-10-27 11:16:24 +0100

    changed the Odner overview

--- src/components/ComponentUI.jsx
+++
@@ -1,9 +0,0 @@
-// Header Komponente
-function Header({ image, title }) {
-  return (
-    <header className="header">
-      <img src={image} alt="Header" className="header-image" />
-      <h1>{title}</h1>
-    </header>
-  );
-}
